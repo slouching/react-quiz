ebb2d1d9d8363dfa27846c174abf30902c63e3c3
Slouching <dulnik@yandex.ru>
2020-01-22 20:37:23 +0300

    first commit

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from './Button.module.scss'
+
+const Button = props => {
+  return (
+    <button
+      className={classes.Button}
+    >
+      Повторить
+    </button>
+  )
+};
+
+export default Button
